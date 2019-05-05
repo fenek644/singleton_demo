a2a5c1f85723161f7eed467a875e216474d764f4
fenek644 <fenek644@ya.ru>
2019-05-05 22:15:16 +0300

    Refactorin : extract logger to separate file

--- app.rb
+++ app.rb
@@ -1,28 +1,3 @@
-class Logger
-  def initialize
-    @f = File.open "log.txt", 'a'
-  end
-
-  def self.instance
-    @@x ||= Logger.new
-  end
-
-  def self.say_smth
-    puts 'something'
-  end
-  def log_smth what
-    # f = File.open 'log.txt', 'a'
-    @f.puts what
-    # f.close
-  end
-
-end
-
-
-Logger.say_smth
-# Logger.log_smth "Loren ipsum"
-
-log = Logger.new
-log.log_smth "Loren"
+require './logger'
 
 Logger.instance.log_smth"instance"
---
+++ logger.rb
@@ -0,0 +1,18 @@
+class Logger
+  def initialize
+    @f = File.open "log.txt", 'a'
+  end
+
+  @@x = Logger.new
+
+  def self.instance
+    @@x
+  end
+
+  def log_smth what
+    @f.puts what
+  end
+
+  private_class_method :new
+
+end
