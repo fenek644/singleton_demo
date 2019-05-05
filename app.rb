70815ee980c17e493933b4c3b00f5be997dab8df
fenek644 <fenek644@ya.ru>
2019-05-05 19:58:14 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 class Logger
+  def initialize
+    @f = File.open "log.txt", 'a'
+  end
   def self.say_smth
     puts 'something'
   end
+  def log_smth what
+    # f = File.open 'log.txt', 'a'
+    @f.puts what
+    # f.close
+  end
+
 end
 
 
 Logger.say_smth
+# Logger.log_smth "Loren ipsum"
+
+log = Logger.new
+log.log_smth "Loren"
 
